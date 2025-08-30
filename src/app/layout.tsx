87f9107cc9fc6f386089ea81ff6e2d210ce1cde9
PruebaPuka Lan <pruebapukalan@gmail.com>
2025-08-30 07:41:22 +0000

    no muestra nada que diga agregar a la pantalla de inicio o instalar apli

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,12 +19,16 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.React.Node;
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
         <meta name="theme-color" content="#00BFFF" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="BiruTracker" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
       </head>
       <body className={`${ptSans.variable} font-body antialiased`}>
         <AuthProvider>
